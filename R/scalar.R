64058cd51d1025db35d806a3a90503dbd0f783c4
Joe Thorley <joe@poissonconsulting.ca>
2017-11-25 21:47:16 -0800

    check_count coerce = TRUE

--- R/scalar.R
+++ R/scalar.R
@@ -47,9 +47,10 @@ check_flag <- function(x,
 
 #' Check Count
 #'
-#' Checks if object is a non-negative integer.
+#' Checks if object is a non-negative integer (or non-negative real whole number).
 #'
 #' @param x The object to check.
+#' @param coerce A flag indicating whether to coerce a non-negative numeric (real) whole number to a count.
 #' @param x_name A string of the name of the object.
 #' @param error A flag indicating whether to throw an informative error or immediately generate an informative message if the check fails.
 #' @return An invisible copy of x (if it doesn't throw an error).
@@ -59,13 +60,20 @@ check_flag <- function(x,
 #' check_count(-1L, error = FALSE)
 #' check_count(1L, error = FALSE)
 #' check_count(1, error = FALSE)
-check_count <- function(x,
+#' check_count(1, coerce = TRUE, error = FALSE)
+#' check_count(1.01, coerce = TRUE, error = FALSE)
+check_count <- function(x, coerce = FALSE,
                        x_name = substitute(x),
                        error = TRUE) {
   x_name <- deparse_x_name(x_name)
 
+  check_flag_internal(coerce)
   check_flag_internal(error)
 
+  if(coerce && is.numeric(x) && identical(as.numeric(as.integer(x)), x)) {
+    x <- as.integer(x)     
+  }
+  
   check_vector(x, values = c(0L, .Machine$integer.max), min_length = 1, max_length = 1,
                x_name = x_name, error = error)
 }
--- tests/testthat/test-count.R
+++ tests/testthat/test-count.R
@@ -11,3 +11,11 @@ test_that("check_count errors", {
   expect_error(check_count(y), "y must be class integer")
   expect_error(check_count(-1L), "the values in -1L must lie between 0 and 2147483647")
 })
+
+test_that("check_count coercion", {
+  expect_error(check_count(1), "1 must be class integer")
+  expect_identical(check_count(1, coerce = TRUE), 1L)
+  expect_error(check_count(1.01, coerce = TRUE), "1.01 must be class integer")
+  expect_identical(check_count(1.000000000000000000000001, coerce = TRUE), 1L)
+})
+
