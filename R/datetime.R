f2c27d54bc922056dcd5776e3249cd83cf5ecaaa
Joe Thorley <joe@poissonconsulting.ca>
2017-11-28 12:12:32 -0800

    check datetime

--- R/datetime.R
+++ R/datetime.R
@@ -1,31 +1,23 @@
-#' Check Date
+#' Check Date Time
 #' 
-#' Checks if x is a date (non-missing Date scalar).
+#' Checks if x is a datetime (non-missing POSIXt scalar).
 #'
 #' @param x The object to check.
 #' @param x_name A string of the name of the object.
-#' @param coerce A flag indicating whether to coerce a date time (POSIXt scalar) to a Date.
 #' @param error A flag indicating whether to throw an informative error or immediately generate an informative message if the check fails.
 #' @return An invisible copy of x (if it doesn't throw an error).
 #' @export
 #'
 #' @examples
-#' check_date(Sys.Date(), error = FALSE)
-#' check_date(Sys.time(), error = FALSE)
-#' check_date(Sys.time(), coerce = TRUE, error = FALSE)
-check_date <- function(x,
-                       coerce = FALSE,
+#' check_datetime(Sys.Date(), error = FALSE)
+#' check_datetime(Sys.time(), error = FALSE)
+check_datetime <- function(x,
                        x_name = substitute(x),
                        error = TRUE) {
   x_name <- deparse_x_name(x_name)
   
-  check_flag_internal(coerce)
   check_flag_internal(error)
   
-  if(coerce && is.POSIXt(x)) {
-    x <- as.Date(x, tz = tz(x))
-  }
-  
-  check_vector(x, values = as.Date("2001-01-01"), length = 1,
+  check_vector(x, values = as.POSIXct("2001-01-01"), length = 1,
                x_name = x_name, error = error)
 }
---
+++ tests/testthat/test-datetime.R
@@ -0,0 +1,15 @@
+context("check-datetime")
+
+test_that("check_datetime errors", {
+  y <- as.POSIXct("2001-02-03 23:59:59", tz = "PST8PDT")
+  expect_identical(check_datetime(y), y)
+  y <- c(y,y)
+  expect_error(check_datetime(y), "y must have 1 element")
+  y <- 2
+  expect_error(check_datetime(y), "y must be class POSIXt")
+
+  y <- as.Date("2002-01-02")
+  expect_error(check_datetime(y), "y must be class POSIXt")
+  y <- NULL
+  expect_error(check_datetime(y), "y must be class POSIXt")
+})
