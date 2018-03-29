c974ba511ed61c6b54f2457ee25395e3b3096e17
Joe Thorley <joe@poissonconsulting.ca>
2018-03-29 16:43:55 -0700

    more data tests

--- R/data.R
+++ R/data.R
@@ -48,6 +48,7 @@ check_data <- function(x,
   
   if(!missing(values)) {
     if(is.list(values)) {
+      check_colnames(x, colnames = names(values), x_name = x_name)
       check_colnames(x, colnames = names(values), x_name = x_name, 
                      exclusive = exclusive, order = order, error = error)
 
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -13,8 +13,20 @@ test_that("check_data factor", {
 
 test_that("check_data ordered", {
   x <- data.frame(factor = ordered(1:3))
+  expect_identical(check_data(x, values = list(factor = ordered(1:3))), x)
   expect_identical(check_data(x, values = list(factor = factor(1:3))), x)
   x <- data.frame(factor = factor(1:3))
   expect_error(check_data(x, values = list(factor = ordered(1:3))), 
                "column factor of x must be class ordered")
+  x <- data.frame(factor = ordered(1:3))
+  expect_identical(check_data(x, values = list(factor = ordered(1:4))), x)
+})
+
+test_that("check_data ordered", {
+  x <- data.frame(factor = ordered(1:3))
+  expect_error(check_data(x, values = list(factor = ordered(c(1,3)))), 
+               "level sets of factors are different")
+  expect_identical(check_data(x, values = list(factor = ordered(c(1,3), 1:3))), x) 
+  expect_error(check_data(x, values = list(factor = ordered(c(1,2), 1:3))), "the values in column factor of x must lie between '1' and '2'") 
+  expect_identical(check_data(x, values = list(factor = ordered(c(3, 1,2), 1:3))), x) 
 })
