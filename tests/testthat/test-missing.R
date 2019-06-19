6186ca2858f5ea2ae411963f57736b38b3ac3edf
Evan <evanamiesgalonski@gmail.com>
2019-06-19 12:04:06 -0700

    test check_missing_names

--- tests/testthat/test-missing.R
+++ tests/testthat/test-missing.R
@@ -6,3 +6,8 @@ test_that("missing", {
   expect_error(check_missing_colnames(data, c("y", "x", "a")), "data must not have columns 'x' and 'y'")
 })
   
+test_that("missing names", {
+  vec <- c(x = 1, y = 2, z = 0)
+  expect_error(check_missing_names(vec, c("x", "y", "a"), error = TRUE))
+  expect_identical(check_missing_names(vec, "a", error = TRUE), vec)
+})
