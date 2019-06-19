5844d9e83dc9382cae045cde29206c7a0d87378a
Evan <evanamiesgalonski@gmail.com>
2019-06-19 12:15:11 -0700

    test int pos/neg

--- tests/testthat/test-int.R
+++ tests/testthat/test-int.R
@@ -10,3 +10,15 @@ test_that("check_int errors", {
   y <- NA_integer_
   expect_error(check_int(y), "y must not include missing values")
 })
+
+test_that("check_pos_int", {
+  expect_identical(check_pos_int(1L, error = FALSE), 1L)
+  expect_error(check_pos_int(0, error = TRUE))
+  expect_error(check_pos_int(1:2, error = TRUE))
+})
+
+test_that("check_neg_int", {
+  expect_identical(check_neg_int(-1L, error = FALSE), -1L)
+  expect_error(check_neg_int(0, error = TRUE))
+  expect_error(check_neg_int(-1:-2, error = TRUE))
+})
