2c52b29014841d369398dfb823edc2df5725bd84
Ayla Pearson <ayla@poissonconsulting.ca>
2021-09-20 16:29:27 -0700

    styling check_nchar and updating deprecated help message to include chk_equal with nchar over check_match

--- R/nchar.R
+++ R/nchar.R
@@ -1,19 +1,28 @@
 #' Check Number of Characters
-#' 
+#'
 #' Checks the number of characters in the elements of an object.
 #'
 #' @inheritParams params
-#' @param nchar A flag indicating whether x should have characters or a missing value indicating no requirements or a count or count range of the number of characters.
-#' 
+#' @param nchar A flag indicating whether x should have characters or a missing
+#'   value indicating no requirements or a count or count range of the number of
+#'   characters.
+#'
 #' @return An invisible copy of x (if it doesn't throw an error).
 #' @seealso \code{\link{check_pattern}} and \code{\link{check_regex}}
 #' @export
 #'
 #' @examples
 #' check_nchar(c("foo", "bar"), nchar = 3)
-check_nchar <- function(x, nchar = TRUE, x_name = substitute(x),
-                         error = TRUE) {
-  lifecycle::deprecate_soft("0.5.1", "check_nchar()", "chk::chk_match()")
+check_nchar <- function(x, 
+                        nchar = TRUE, 
+                        x_name = substitute(x),
+                        error = TRUE) {
+  lifecycle::deprecate_soft(
+    "0.5.1", 
+    "check_nchar()", 
+    NULL, 
+    "Please use `chk::chk_equal()` with nchar() or `chk::chk_match()` instead."
+  )
   x_name <- chk_deparse(x_name)
 
   check_flag_internal(error)
